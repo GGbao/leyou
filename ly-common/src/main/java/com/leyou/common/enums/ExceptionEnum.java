93800d8fbbc95a59cf778aa1bca9d85c8361f19b
GGbao <379160943@qq.com>
2019-07-19 19:57:13 +0800

    采用cors解决跨域问题

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public enum ExceptionEnum {
     PRICE_CANNOT_BE_NULL(400, "价格不能为空！"),//此处定义类别对象
+    CATEGORY_NOT_FOUND(404, "商品分类未查询到"),
     ;                                                     //此处封号必须的
     private int code;
     private String msg;
