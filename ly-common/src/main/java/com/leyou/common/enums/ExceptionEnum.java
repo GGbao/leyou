2764207d3bc6e4f370c923ef7c3c50a5f65e68f6
GGbao <379160943@qq.com>
2019-07-22 21:46:59 +0800

    ##实现品牌新增  #搭建FastFDS来解决上传图片问题

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -8,9 +8,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum ExceptionEnum {
-    PRICE_CANNOT_BE_NULL(400, "价格不能为空！"),//此处定义类别对象
+    BRAND_NOT_FOUND(404, "品牌不存在"),//此处定义类别对象
     CATEGORY_NOT_FOUND(404, "商品分类未查询到"),
+    BRAND_SAVE_ERROR(500, "新增品牌失败"),
+    UPLOAD_FILE_ERROR(500, "文件上传失败"),
+    INVALID_FILE_TYPE(400, "无效的文件类型"),
     ;                                                     //此处封号必须的
     private int code;
     private String msg;
 }
+
